61a7b568259a1894de1d851839c1e4bf53673ffc
Hiren Bambhaniya <148859956+HirenCodeMaster11@users.noreply.github.com>
2024-01-26 13:03:13 +0530

    Add files via upload

---
+++ INTRO/feet to inche.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+//Q.3 Write a program to read and print distance for feet and inches.
+
+main()
+{
+	int feet,inche;
+	int i;
+	
+	cout << "Enter the Feet : ";
+	cin >> feet;
+	cout << "Enter the Inche : ";
+	cin >> inche;
+	
+	cout << endl;
+	
+	for(i=1; i<=inche; i++)
+	{
+		if(inche==12)
+		{
+			inche-=12;
+			feet++;	
+		}
+	}
+	
+	cout << "Feet is : " << feet << endl;
+	cout << "Inche is : " << inche;
+}
